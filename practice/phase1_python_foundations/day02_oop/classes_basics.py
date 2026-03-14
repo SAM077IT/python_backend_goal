c8361d2a053a52c0ca1018bfb31ed9870716ab4e
Samirul Islam <samirul077islam@gmail.com>
2026-03-15 00:56:20 +0530

    Initial roadmap: Practice structure added

---
+++ practice/phase1_python_foundations/day02_oop/classes_basics.py
@@ -0,0 +1,26 @@
+# OOP - Day 2: Classes & Inheritance
+# What I learned: A class is a blueprint. __init__ runs on object creation.
+
+class Animal:
+    def __init__(self, name, sound):
+        self.name = name
+        self.sound = sound
+
+    def __str__(self):
+        return f"{self.name} says {self.sound}"
+
+    def speak(self):
+        print(self.__str__())
+
+
+class Dog(Animal):
+    def __init__(self, name):
+        super().__init__(name, "Woof")  # super() calls parent __init__
+
+    def fetch(self):
+        print(f"{self.name} fetches the ball!")
+
+
+dog = Dog("Bruno")
+dog.speak()     # Bruno says Woof
+dog.fetch()     # Bruno fetches the ball!
